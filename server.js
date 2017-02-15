71afe222954af5a7f4bab730ad5facef0606a51e
DESKTOP-84IKM2Q\Desarrollo <ketrujillo08@gmail.com>
2017-02-15 15:24:32 -0500

    Update middleware for locahost

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ var express = require('express');
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(function(req,res,next){
+	if(req.header['x-forwarded-proto'] === 'http')
+	{
+		res.redirect('http://'+req.hostname + req.url);
+		
+	}else
+	{
+		next();
+	}
+});
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port '+PORT);
 });
+
+
